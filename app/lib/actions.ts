23a257293e273957a9ab9f900ab623d065016535
John Mark Manuel <manueljm016@gmail.com>
2025-01-25 11:20:57 +0800

    updated login error message

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,7 +110,7 @@ export async function authenticate(
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return 'Invalid credentials.';
+          return 'Invalid Credentials.';
         default:
           return 'Something went wrong.';
       }
